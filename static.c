fcefa8ea21ef6c6117772717ce1113328fe2071c
hadizorkot <102998872+hadizorkot@users.noreply.github.com>
2023-10-12 20:46:47 +0300

    Add files via upload

---
+++ static.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define WIDTH 1200
+#define HEIGHT 1200
+
+int main(int argc, char** argv) {
+    double communication_time;
+    MPI_Init(&argc, &argv);
+    double start_time = MPI_Wtime();
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    double real_min = -2.0;
+    double real_max = 1.0;
+    double imag_min = -1.0;
+    double imag_max = 1.0;
+    int max_iterations = 1000;
+
+    int rows_per_process = HEIGHT / size;
+    int start_row = rows_per_process * rank;
+    int end_row = start_row + rows_per_process;
+
+    if (rank == size - 1) {
+        end_row = HEIGHT;
+    }
+
+    int* row = (int*)malloc(sizeof(int) * WIDTH);
+    int* local_data = (int*)malloc(sizeof(int) * WIDTH * rows_per_process);
+
+    for (int y = start_row; y < end_row; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            double c_real = real_min + (real_max - real_min) * x / WIDTH;
+            double c_imag = imag_min + (imag_max - imag_min) * y / HEIGHT;
+
+            double z_real = 0.0;
+            double z_imag = 0.0;
+
+            int iter = 0;
+            while (z_real * z_real + z_imag * z_imag < 4.0 && iter < max_iterations) {
+                double next_z_real = z_real * z_real - z_imag * z_imag + c_real;
+                double next_z_imag = 2.0 * z_real * z_imag + c_imag;
+                z_real = next_z_real;
+                z_imag = next_z_imag;
+                iter++;
+            }
+
+            if (iter == max_iterations) {
+                row[x] = 0;
+            } else {
+                row[x] = iter % 256;
+            }
+        }
+
+        int row_index = (y - start_row) * WIDTH;
+        for (int x = 0; x < WIDTH; x++) {
+            local_data[row_index + x] = row[x];
+        }
+    }
+
+    free(row);
+
+    int* global_data = NULL;
+    if (rank == 0) {
+        global_data = (int*)malloc(sizeof(int) * WIDTH * HEIGHT);
+    }
+
+    double start_communication_time = MPI_Wtime();
+    MPI_Gather(local_data, WIDTH * rows_per_process, MPI_INT, global_data, WIDTH * rows_per_process, MPI_INT, 0, MPI_COMM_WORLD);
+    double end_communication_time = MPI_Wtime();
+    communication_time = end_communication_time - start_communication_time;
+
+    free(local_data);
+
+    if (rank == 0) {
+        FILE* file_ptr = fopen("mandelbrot.pgm", "wb");
+        fprintf(file_ptr, "P5\n%d %d\n255\n", WIDTH, HEIGHT);
+        for (int i = 0; i < WIDTH * HEIGHT; i++) {
+            fputc(global_data[i], file_ptr);
+        }
+        fclose(file_ptr);
+        free(global_data);
+    }
+
+    double end_time = MPI_Wtime();
+    double elapsed_time = end_time - start_time;
+    printf("Elapsed time: %f seconds\n", elapsed_time);
+
+    printf("Communication time: %f seconds\n", communication_time);
+    MPI_Finalize();
+    return 0;
+}
